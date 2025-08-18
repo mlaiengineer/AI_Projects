c775ad4754e3123fe610af4031e3618c5f7a6240
Abdulbaset Saleh <155334992+mlaiengineer@users.noreply.github.com>
2025-08-18 10:20:32 +0300

    Update main.py

--- FirstProject/main.py
+++ FirstProject/main.py
@@ -5,23 +5,11 @@ from langgraph.prebuilt import create_react_agent
 from dotenv import load_dotenv
 
 load_dotenv()
-
-@tool
-def calculator(first_number: float, second_number: float) -> str:
-    """Useful for performing basic arithmetic calculations with numbers"""
-
-    return f"The sum of {first_number} and {second_number} is {first_number + second_number}"
-
-@tool
-def greeting_user(name: str) -> str:
-    """Useful for greeting a user"""
-
-    return f"Hi {name} 😍, I hope you're well today"
 def main():
 
     model = ChatOpenAI(temperature=0)
 
-    tools = [calculator, greeting_user]
+    tools = []
     agent_executor = create_react_agent(model, tools)
 
     print("Welcome😍! I'm your AI assistant, type 'quit' to exit.")
@@ -31,11 +19,12 @@ def main():
         user_input = input("\nYou: ").strip()
 
         if user_input == 'quit':
+            print("Thanks 😍 for chat with me 🤖🍵, this chat is ended🫡")
             break
 
-        print("\nYour Assistant: ", end="")
+        print("\nYour Assistant:", end="")
         for chunk in agent_executor.stream(
-                {'messages': [HumanMessage(content=user_input)]}
+                {"messages": [HumanMessage(content=user_input)]}
         ):
             if 'agent' in chunk and 'messages' in chunk['agent']:
                  for message in chunk['agent']['messages']:
